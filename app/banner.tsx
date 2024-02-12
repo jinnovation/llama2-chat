90d1018c76935bea7de82f6cbcdc27d3335e9b2b
Jonathan Jin <me@jonathanj.in>
2024-02-12 10:06:20 -0600

    fix faulty jsx

--- app/banner.tsx
+++ app/banner.tsx
@@ -2,7 +2,6 @@
 
 export default function Banner() { 
     return <div id="banner">
-        This is JJin's Llama2 chat proof-of-concept.
-        Expect rough edges.
+        {"This is JJin's Llama2 chat proof-of-concept. Expect rough edges."}
     </div>
 }
